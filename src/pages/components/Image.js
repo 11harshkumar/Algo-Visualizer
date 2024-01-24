2a5a1bbbb8d5f67c47c9d205f9042d6b7d20d8f0
Harsh <76443685+11harshkumar@users.noreply.github.com>
2024-01-24 22:40:18 +0530

    new Changes

--- src/pages/components/Image.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./../../css/Image.css";
-import Img from "./../../images/arrowImg.png";
-
-export default function Image() {
-	return <img className="Image" src={Img} alt="arrow" />;
-}
